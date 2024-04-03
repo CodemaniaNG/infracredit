57df6ce4489899416fe28a36202375891e1cbafe
Mike <michael.u@codemania.com.ng>
2024-04-03 14:27:24 +0100

    fixes

--- src/pages/editor/[template].tsx
+++ src/pages/editor/[template].tsx
@@ -39,6 +39,7 @@ const Editor = () => {
   const [isOpen5, setIsOpen5] = useState(false);
   const [isOpen6, setIsOpen6] = useState(false);
   const [isOpen7, setIsOpen7] = useState(false);
+  const [isOpen8, setIsOpen8] = useState(false);
   const [file, setFile] = useState<any>(null);
 
   const handleModal = () => {
@@ -69,6 +70,10 @@ const Editor = () => {
 
   const handleModal7 = () => {
     setIsOpen7(!isOpen7);
+  };
+
+  const handleModal8 = () => {
+    setIsOpen8(!isOpen8);
   };
 
   return (
@@ -120,6 +125,7 @@ const Editor = () => {
                     color="greens.100"
                     borderWidth="1px"
                     px={6}
+                    onClick={handleModal8}
                   />
                 )}
                 <Button
@@ -394,6 +400,124 @@ const Editor = () => {
               fontFamily={"body"}
             >
               Share Contract
+            </Text>
+
+            <VStack w={"100%"} align="flex-start">
+              <Text
+                color={"subText.200"}
+                fontSize={"14px"}
+                fontWeight={"500"}
+                fontFamily={"body"}
+              >
+                Title
+              </Text>
+              <VStack
+                w={"100%"}
+                align="flex-start"
+                bg="bg.100"
+                p={3}
+                borderRadius={8}
+              >
+                <Text
+                  color={"secondary"}
+                  fontSize={"12px"}
+                  fontWeight={"500"}
+                  fontFamily={"body"}
+                >
+                  Contractual Agreement | January 3, 2023
+                </Text>
+              </VStack>
+            </VStack>
+            <Formik
+              initialValues={{
+                description: "",
+                recipients: ["colaborator1@gmail.com"],
+                comment: "",
+              }}
+              onSubmit={(values, actions) => {
+                console.log(values);
+              }}
+            >
+              {(props) => (
+                <Form style={{ width: "100%" }}>
+                  <VStack>
+                    <Input
+                      label="Document Description"
+                      name="description"
+                      type="text"
+                      placeholder="Document Description"
+                    />
+
+                    <FieldArray
+                      name="recipients"
+                      render={(arrayHelpers) => (
+                        <VStack align="stretch" w={"100%"} mt={4}>
+                          {props.values.recipients.map((email, index) => (
+                            <VStack key={index} position="relative">
+                              <Input
+                                label="Recipient’s Email"
+                                name={`recipients.${index}`}
+                                type="email"
+                                placeholder="Recipient’s Email"
+                              />
+                              {index > 0 && (
+                                <IconButton
+                                  aria-label="delete recipient"
+                                  position="absolute"
+                                  right="-2"
+                                  top="-2"
+                                  icon={<FiTrash2 size={20} color="#FF3B30" />}
+                                  onClick={() => arrayHelpers.remove(index)}
+                                  variant="ghost"
+                                  _hover={{ bg: "transparent" }}
+                                />
+                              )}
+                            </VStack>
+                          ))}
+                          <Button
+                            text="Add another recipient"
+                            size="sm"
+                            fontSize={10}
+                            onClick={() => arrayHelpers.push("")}
+                            variant="outline"
+                            bg="transparent"
+                            color="subText.400"
+                            border="border.100"
+                            borderStyle="dashed"
+                          />
+                        </VStack>
+                      )}
+                    />
+
+                    <Input
+                      label="Comment for Recipient"
+                      name="comment"
+                      type="text"
+                      placeholder="Comment for Recipient"
+                    />
+
+                    <VStack align="stretch" w={"100%"} mt={4}>
+                      <Button text="Share" px={4} py={4} type="submit" />
+                    </VStack>
+                  </VStack>
+                </Form>
+              )}
+            </Formik>
+          </VStack>
+        }
+      />
+      <Modal
+        isOpen={isOpen8}
+        onClose={handleModal8}
+        body={
+          <VStack align="flex-start" spacing={4} mt={10} mb={5}>
+            <Text
+              color={"greens.100"}
+              fontSize={"24px"}
+              fontWeight={"600"}
+              fontFamily={"body"}
+            >
+              Share Report
             </Text>
 
             <VStack w={"100%"} align="flex-start">
